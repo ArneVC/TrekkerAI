4ad89d00e576746d173dbc2e059232547a2b9ede
Arne <arnev.c.23@gmail.com>
2024-01-04 22:48:12 +0100

    export model 1.0

--- model/TrekkerAIModel/Model_1.0/Program.cs
+++ model/TrekkerAIModel/Model_1.0/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.ML;
+using System;
+
+namespace Model_1_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load the trained model
+            var mlContext = new MLContext();
+            var model = mlContext.Model.Load("Model_1.0.mlnet", out var modelSchema);
+
+            // Save the model as ONNX
+            mlContext.Model.Save(model, modelSchema, "Model_1.0.onnx");
+        }
+    }
+}
